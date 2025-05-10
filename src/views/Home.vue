4b30be7e07a4207e1965c9d9eb5f8ffd8ab1ddd2
Berqueno <berqueno@gmail.com>
2025-05-10 21:37:00 +0300

    Setup Vue.js

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hoş Geldin</h1>
+    <p>Burası anasayfa.</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
